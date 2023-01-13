dbd7863afb91240a71ce5761342727ba03991c05
fay-muema <faithmuema45@gmail.com>
2023-01-13 19:03:17 +0300

    flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+""" Script that runs an app with Flask framework """
 from flask import Flask
-"""flask which created two routes"""
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """function too print hello"""
+def hello_hbnb():
+    """ Function called with / route """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """second function"""
+    """ Function called with /hbnb route """
     return 'HBNB'
 
-
 if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
