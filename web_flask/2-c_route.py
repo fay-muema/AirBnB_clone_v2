4a27fe2e265215c480bb821a4194803d5cdb7d07
fay-muema <faithmuema45@gmail.com>
2023-01-13 16:11:25 +0300

    adding text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,28 +4,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-
-
 def hello():
-    """function too print hello"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-
-
 def hbnb():
-    """second function"""
     return 'HBNB'
 
-@app.route('/c', strict_slashes=False)
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    
 
-    return render_template('index.html', processed_text=processed_text)
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C %s' % text.replace('_', ' ')
+
+
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
-
