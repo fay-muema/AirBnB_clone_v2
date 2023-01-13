7c4307b176f4574adeeb917ae1178981fbd23a56
fay-muema <faithmuema45@gmail.com>
2023-01-13 00:53:42 -0800

    script to run in a specific port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+""" Script for running flask"""
+
 from flask import Flask
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-@app.route('/')
+
+
+@app.route('/', strict_slashes=False)
 def hello():
+    """Function called with route function"""
     return 'Hello HBNB!'
+
+
+if ___name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
