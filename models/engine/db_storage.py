f16589cf5ed2e722b78b3d15e2a4d5ff4aa273c0
fay-muema <faithmuema45@gmail.com>
2023-01-14 12:37:18 +0300

    json

---
+++ models/engine/db_storage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def close(self):
+    """function that calls remove"""
+    self.__session.close()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def close(self):
+    """calls reload method"""
+    sel.reload():
