3a85cb64c2e19699bca421613224a734bb72c0ee
fay-muema <faithmuema45@gmail.com>
2023-01-15 12:31:39 +0300

    creating database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
---creating database
---granting priviledes
-
-CREATE DATABASE IF NOT EXISTS hbnb_test_db
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+-- Creating hbnb_dev_db database
+-- Privileges for new user (hbnb_dev)
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
